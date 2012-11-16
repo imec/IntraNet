258e47b21a8538a6536d9570a8246a99744bc3d5
vic9511 <vic9511@hotmail.com>
2012-11-15 18:50:40 -0600

    pruebas

--- application/forms/AgregarCurso.php
+++ application/forms/AgregarCurso.php
@@ -5,28 +5,27 @@ class Application_Form_AgregarCurso extends Zend_Form {
     public function init() {
 
         $this->addElement(
-                'text', 'codigo', array(
-					'label' => 'Codigo',
-					'required' => true
-                )
-        );
-		
-		$this->addElement(
-                'text', 'nombre', array(
-					'label' => 'Nombre',
-					'required' => true
-                )
-        );
-		
-        $this->addElement(
-                'textarea', 'descripcion', array(
-					'label' => 'Descripcion'
-                )
+            'text', 'codigo', array(
+                'label' => 'Codigo',
+                'required' => true
+            )
         );
 
         $this->addElement(
-                'submit', 'Guardar', array()
+            'text', 'nombre', array(
+                'label' => 'Nombre',
+                'required' => true
+            )
+        );
+
+        $this->addElement(
+            'textarea', 'descripcion', array(
+                'label' => 'Descripcion'
+            )
+        );
+
+        $this->addElement(
+            'submit', 'Guardar', array()
         );
     }
-
 }
